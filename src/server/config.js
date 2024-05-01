a8dc8b7a629fccac28a92b609e65c6baca057404
Petar Nenov <petarnenovpetrov@gmail.com>
2024-05-01 21:28:12 +0300

    fix: cors

--- src/server/config.js
+++ src/server/config.js
@@ -2,7 +2,7 @@ const cors = {
      
     origin:
       process.env.NOD_ENV === "production"
-        ? ["http://192.168.0.164:5173"]
+        ? ["http://192.168.0.164:5173","http://localhost:5173"]
         : ["http://localhost:5173", "http://127.0.0.1:5173", "http://192.168.0.164:5173"],
     credentials: true,
   };
